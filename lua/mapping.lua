e43a5e13358c2a0b404879067f702fab45172a2a
Philippe quesnel <philippe.quesnel@gmail.com>
2020-12-31 16:20:34 -0500

    wip, testbed & note

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -12,30 +12,51 @@ theKeyboard = {
 function theKeyboard:addMapping(fromChar, toChar)
 	-- (#todo get real scancode / vk etc)
 	-- convert string to byte 
+
+	--pqLayout.GetVk :
+	-- table["vk"] = vk;
+	-- table["shiftOn"] = (flags & 1) != 0;
+	-- table["ctrlOn"] = (flags & 2) != 0;
+	-- table["altOn"] = (flags & 4) != 0;
+
+	vkFromTbl = pqLayout.GetVk(fromChar)
+	vkToTbl = pqLayout.GetVk(toChar)
+
 	print(string.format("addMapping from=%s, tp=%s", fromChar, toChar))
+	print(string.format("           from=%x, tp=%x", vkFromTbl["vk"], vkToTbl["vk"]))
 
-	vkFrom = pqLayout.GetVk(fromChar)
-	vkTo = pqLayout.GetVk(toChar)
-
-	-- self.mappings[vkFrom] = vkTo
+	self.mappings[vkFromTbl["vk"]] = vkToTbl
 end
 
 function theKeyboard:getMapping(vkFrom)
-	return self.mappings[vkFrom]
+	vkToTbl = self.mappings[vkFrom]
+	if vkToTbl == nil then
+		return nil
+	end
+	
+	vkTo = vkToTbl["vk"]
+	if vkToTbl["shiftOn"] then
+		vkTo = vkTo | 0x1000
+	end
+	return vkTo
 end
 
 -- called from C++ for wm_keydown/up
 function theKeyboard:onKey(vk, down)
-	print(string.format("theKeyboard:OnKey vk=%x", vk))
+	print(string.format("theKeyboard:OnKey vk=%x, down=%s", vk, down))
 	return self:getMapping(vk)
 end
 
 -------------------
 
-theKeyboard:addMapping('A','G')
-theKeyboard:addMapping('S','I')
-theKeyboard:addMapping('D','O')
-theKeyboard:addMapping('F',' ')
+theKeyboard:addMapping('A','Y')
+theKeyboard:addMapping('S','E')
+theKeyboard:addMapping('D',' ')
+theKeyboard:addMapping('F','A')
+theKeyboard:addMapping('G','G')
+
+theKeyboard:addMapping('Z','.')
+theKeyboard:addMapping('C',':')
 
 -- theKeyboard:addMapping('','')
 
